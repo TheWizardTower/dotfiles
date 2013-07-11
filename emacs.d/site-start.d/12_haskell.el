05fcad8b52941e610e402ad1186be2a7b9dc3754
Ted Reed <ted.reed@gmail.com>
2013-07-11 11:50:48 -0700

    Adding a haskell library, as well as haskell + flymake support to emacs.

---
+++ emacs.d/site-start.d/12_haskell.el
@@ -0,0 +1,8 @@
+(load "~/dotfiles/haskell-mode/haskell-site-file")
+
+
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+
