3339de7e6730f206a93b1ae1e827ab23a7ba8231
Adam McCullough <merlinfmct87@gmail.com>
2015-07-18 14:36:09 -0700

    New format!

--- fish/config.fish
+++ fish/config.fish
@@ -1,19 +1,22 @@
 # Path to your oh-my-fish.
 set fish_path $HOME/.oh-my-fish
 
-# Theme
-set fish_theme agnoster-mercurial
-
-# All built-in plugins can be found at ~/.oh-my-fish/plugins/
-# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
-# Enable plugins by adding their name separated by a space to the line below.
-set fish_plugins theme tmux
-
 # Path to your custom folder (default path is ~/.oh-my-fish/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
 
 # Load oh-my-fish configuration.
 . $fish_path/oh-my-fish.fish
+
+
+# Theme
+Theme "agnoster-mercurial"
+
+# All built-in plugins can be found at ~/.oh-my-fish/plugins/
+# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
+# Enable plugins by adding their name separated by a space to the line below.
+Plugin "theme"
+Plugin "tmux"
+
 
 setenv SSH_ENV $HOME/.ssh/environment
 
