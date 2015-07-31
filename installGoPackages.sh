fd49b888294a8feb50caaaeedd505d7aff4c0f4b
Adam McCullough <merlinfmct87@gmail.com>
2015-07-31 01:32:47 -0700

    Adding some more go sanity checking goodness.

--- installGoPackages.sh
+++ installGoPackages.sh
@@ -2,8 +2,9 @@
 
 export GOPATH="/home/amccullough/gocode"
 
-# go get -u all
 go get -u github.com/golang/lint/golint
 go get -u github.com/dougm/goflymake
 go get -u github.com/nsf/gocode
+go get -u github.com/kisielk/errcheck
 go get -u github.com/peco/peco/cmd/peco
+go get -u golang.org/x/tools/cmd/goimports
