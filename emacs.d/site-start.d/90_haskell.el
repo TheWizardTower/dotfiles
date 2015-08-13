379f8b78b790fce91c0ad66a2de5c437ef1f6bca
Adam McCullough <merlinfmct87@gmail.com>
2015-08-13 03:09:06 -0700

    Adding stuff and things. I'm up too late and reading github, this is a problem.

--- emacs.d/site-start.d/90_haskell.el
+++ emacs.d/site-start.d/90_haskell.el
@@ -1,3 +1,13 @@
 (eval-after-load 'flycheck '(require 'flycheck-ghcmod))
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 
+(defadvice haskell-mode-stylish-buffer (around skip-if-flycheck-errors activate)
+  (unless (flycheck-has-current-errors-p 'error)
+    ad-do-it))
+(setq haskell-stylish-on-save t)
+
+(add-hook 'haskell-mode-hook 'structured-haskell-mode)
+
+(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+;; Ignore compiled Haskell files in filename completions
+(add-to-list 'completion-ignored-extensions ".hi")
--- installCabalPackages.sh
+++ installCabalPackages.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 cabal update
+cabal install ghc-mod
 cabal install cabal-install
 cabal install shellcheck
+cabal install stylish-haskell
+cabal install structured-haskell
