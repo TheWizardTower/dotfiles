59c74ebf9eecc4641dd8c30cf7f34516cfa404ed
Adam McCullough <merlinfmct87@gmail.com>
2015-08-20 23:02:49 -0700

    Fixing a bad package name.

--- installCabalPackages.sh
+++ installCabalPackages.sh
@@ -5,6 +5,6 @@ cabal install ghc-mod
 cabal install cabal-install
 cabal install shellcheck
 cabal install stylish-haskell
-cabal install structured-haskell
+cabal install structured-haskell-mode
 cabal install ghc-imported-from
 cabal install hoogle
