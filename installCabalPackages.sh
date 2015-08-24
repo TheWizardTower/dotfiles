ade5542f6c987d00ae48dc9b24dabb954110804b
Adam McCullough <merlinfmct87@gmail.com>
2015-08-24 13:56:54 -0700

    Install cabal first, to ensure updates get pulled in.

--- installCabalPackages.sh
+++ installCabalPackages.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cabal update
-cabal install ghc-mod
 cabal install cabal-install
+cabal install ghc-mod
 cabal install shellcheck
 cabal install stylish-haskell
 cabal install structured-haskell
