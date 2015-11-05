1d55a5d224d4631b6c015451873055bd1091c428
Adam McCullough <merlinfmct87@gmail.com>
2015-11-04 21:20:54 -0800

    Made the row/column highlight a lot easier on the eyes. Also made line numbers more subtle.

--- emacs.d/site-start.d/00_font.el
+++ emacs.d/site-start.d/00_font.el
@@ -5,5 +5,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 83 :width normal :foundry "unknown" :family "Inconsolata"))))
- '(col-highlight ((t (:background "#073642")))))
-
+ '(linum ((t (:inherit (shadow default) :foreground "dim gray" :width normal))))
+ '(col-highlight ((t (:background "gray15"))))
+ '(hl-line ((t (:background "gray15")))))
