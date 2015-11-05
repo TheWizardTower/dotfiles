bb61249eb82a0a41694c08f0fb7bfd336083e2cc
Adam McCullough <merlinfmct87@gmail.com>
2015-11-04 22:48:01 -0800

    Structured Haskell Mode was a huge nightmare to use.

--- emacs.d/site-start.d/90_haskell.el
+++ emacs.d/site-start.d/90_haskell.el
@@ -1,14 +1,10 @@
 (eval-after-load 'flycheck '(require 'flycheck-ghcmod))
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(add-hook 'haskell-mode-hook 'haskell-indentation-mode)
 
 (defadvice haskell-mode-stylish-buffer (around skip-if-flycheck-errors activate)
   (unless (flycheck-has-current-errors-p 'error)
     ad-do-it))
 (setq haskell-stylish-on-save t)
-
-(add-to-list 'load-path "~/.emacs.d/structured-haskell-mode/elisp/")
-(require 'shm)
-(add-hook 'haskell-mode-hook 'structured-haskell-mode)
 
 (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
 ;; Ignore compiled Haskell files in filename completions
