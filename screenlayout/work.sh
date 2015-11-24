14aac61ab3a492c5f122744667f3cf2a57799f11
Adam McCullough <merlinfmct87@gmail.com>
2015-11-24 13:24:14 -0800

    KDE is shitting itself more, just use xrandr argh.

---
+++ screenlayout/work.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VGA-0 --mode 1920x1200 --pos 544x0 --rotate left --output LVDS-0 --mode 1920x1080 --pos 4144x474 --rotate normal --output DP-5 --mode 1920x1200 --pos 2944x14 --rotate left --output DP-4 --off --output DP-3 --mode 1920x1200 --pos 1744x14 --rotate left --output DP-2 --off --output DP-1 --off --output DP-0 --off
