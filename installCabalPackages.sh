748bb69df573022bf713dce504df477a7b82cc48
Adam McCullough <merlinfmct87@gmail.com>
2015-11-23 19:30:51 -0800

    Never worked right anyway.

--- installCabalPackages.sh
+++ installCabalPackages.sh
@@ -5,6 +5,5 @@ cabal install cabal-install
 cabal install ghc-mod
 cabal install shellcheck
 cabal install stylish-haskell
-cabal install structured-haskell-mode
 cabal install ghc-imported-from
 cabal install hoogle
