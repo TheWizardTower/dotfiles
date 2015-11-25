d401be10f25558b1682079a4b55eb8fd7f796485
Adam McCullough <merlinfmct87@gmail.com>
2015-11-25 01:47:06 -0800

    I'll obsolete this soon with some docker system that builds static binaries. Until then...

--- installCabalPackages.sh
+++ installCabalPackages.sh
@@ -2,9 +2,8 @@
 
 cabal update
 cabal install --avoid-reinstalls --upgrade-dependencies cabal-install
-cabal install --avoid-reinstalls --upgrade-dependencies \
-      ghc-imported-from\
-      ghc-mod \
-      hoogle \
-      shellcheck \
-      stylish-haskell
+cabal install --avoid-reinstalls --upgrade-dependencies ghc-imported-from
+cabal install --avoid-reinstalls --upgrade-dependencies ghc-mod
+cabal install --avoid-reinstalls --upgrade-dependencies hoogle
+cabal install --avoid-reinstalls --upgrade-dependencies shellcheck
+cabal install --avoid-reinstalls --upgrade-dependencies stylish-haskell
