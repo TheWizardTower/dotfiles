f286b19c24ec4431e65d631b7a63362080171952
Adam McCullough <merlinfmct87@gmail.com>
2015-12-15 00:41:58 -0800

    This actually sorta works! Doesn't show up in the popup window list, but 'N' does show git-svn bindings. Sweet.

--- emacs.d/site-start.d/50_magit.el
+++ emacs.d/site-start.d/50_magit.el
@@ -3,6 +3,5 @@
 
 (global-set-key (kbd "C-x G") 'magit-status)
 
-(add-hook 'magit-mode-hook 'magit-svn-mode)
-
 (add-hook 'prog-mode-hook 'turn-on-diff-hl-mode)
+(add-hook 'magit-mode-hook 'magit-svn-mode)
