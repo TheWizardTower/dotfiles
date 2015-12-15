da25dbc45237a89959ae9d341268f8f433c0e5fa
Adam McCullough <merlinfmct87@gmail.com>
2015-12-15 00:43:05 -0800

    Correction, this is what does it.

--- emacs.d/site-start.d/50_magit.el
+++ emacs.d/site-start.d/50_magit.el
@@ -1,7 +1,9 @@
-(require 'magit)
+; (require 'magit)
 (require 'magit-svn)
 
 (global-set-key (kbd "C-x G") 'magit-status)
 
 (add-hook 'prog-mode-hook 'turn-on-diff-hl-mode)
 (add-hook 'magit-mode-hook 'magit-svn-mode)
+
+(global-magit-file-mode)
