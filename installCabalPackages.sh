af9ef985d0ed7f00e89ba9404b1e0aa3417c3b5c
Adam McCullough <merlinfmct87@gmail.com>
2015-12-15 00:20:39 -0800

    More wonderfulness from stack + haskell.

--- installCabalPackages.sh
+++ installCabalPackages.sh
@@ -3,6 +3,9 @@
 stack setup
 stack install ghc-imported-from
 stack install ghc-mod
+stack install HaRe
+stack install hasktags
+stack install hlint
 stack install hoogle
 stack install shellcheck
 stack install stylish-haskell
