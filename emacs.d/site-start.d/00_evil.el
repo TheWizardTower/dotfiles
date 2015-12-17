03480cf1060396ccb4fc2cd31ef448a4a656968a
Adam McCullough <merlinfmct87@gmail.com>
2015-12-16 23:21:44 -0800

    More flexibility to evil, and a better font for coompany mode.

--- emacs.d/site-start.d/00_evil.el
+++ emacs.d/site-start.d/00_evil.el
@@ -16,6 +16,20 @@
 (evil-mode 1)
 
 (require 'evil-args)
+
+;; bind evil-args text objects
+(define-key evil-inner-text-objects-map "a" 'evil-inner-arg)
+(define-key evil-outer-text-objects-map "a" 'evil-outer-arg)
+
+;; bind evil-forward/backward-args
+(define-key evil-normal-state-map "L" 'evil-forward-arg)
+(define-key evil-normal-state-map "H" 'evil-backward-arg)
+(define-key evil-motion-state-map "L" 'evil-forward-arg)
+(define-key evil-motion-state-map "H" 'evil-backward-arg)
+
+;; bind evil-jump-out-args
+(define-key evil-normal-state-map "K" 'evil-jump-out-args)
+
 (require 'evil-tabs)
 (global-evil-tabs-mode t)
 
--- emacs.d/site-start.d/30_yasnippet.el
+++ emacs.d/site-start.d/30_yasnippet.el
@@ -3,5 +3,6 @@
 (require 'helm-c-yasnippet)
 (setq helm-yas-space-match-any-greedy t) ;[default: nil]
 (global-set-key (kbd "C-c y") 'helm-yas-complete)
-(add-hook 'prog-mode-hook #'yas-minor-mode)
+;;(add-hook 'prog-mode-hook #'yas-minor-mode)
+(yas-global-mode)
 (add-to-list 'yas-snippet-dirs "site-lisp/yasnippet-golang")
