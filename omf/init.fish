0492ffb65725da2a506c198443ffc4528e32d9a9
Adam McCullough <merlinfmct87@gmail.com>
2016-02-09 16:11:32 -0800

    Adding changes before wiping machine.

--- omf/init.fish
+++ omf/init.fish
@@ -1,10 +1,10 @@
-set -gx GOPATH $HOME/gocode/:$HOME/code/golang:$HOME/imvu/operations/golang/imvu:/usr/lib/go
+set -gx GOPATH $HOME/gocode/:$HOME/code/golang:$HOME/imvu/operations/golang/imvu:$HOME/imvu/operations/golang/external:/usr/lib/go
 
 set -g OMF_PATH $HOME/.local/share/omf
 set -g OMF_CONFIG $HOME/.config/omf
 
 function fish_title
-  true
+        true
 end
 
 # All built-in plugins can be found at ~/.oh-my-fish/plugins/
@@ -16,41 +16,41 @@ setenv SSH_ENV $HOME/.ssh/environment
 setenv SSH_SOCK_LINK $HOME/.ssh/auth_sock_symlink
 
 function start_agent
-    echo "Initializing new SSH agent ..."
-    set ssh_eval (/usr/bin/ssh-agent -c)
-    echo $ssh_eval > $SSH_ENV
-    echo "succeeded"
-    chmod 600 $SSH_ENV
-    . $SSH_ENV
-    ssh-add ~/.ssh/amccullough-corp-20150911 ~/.ssh/amccullough-prod-20150910
-    ln -sf $SSH_AUTH_SOCK $SSH_SOCK_LINK
+        echo "Initializing new SSH agent ..."
+        set ssh_eval (/usr/bin/ssh-agent -c)
+        echo $ssh_eval > $SSH_ENV
+        echo "succeeded"
+        chmod 600 $SSH_ENV
+        . $SSH_ENV
+        ssh-add ~/.ssh/amccullough-corp-20150911 ~/.ssh/amccullough-prod-20150910
+        ln -sf $SSH_AUTH_SOCK $SSH_SOCK_LINK
 end
 
 function test_identities
-    ssh-add -l | grep -e "The agent has no identities" > /dev/null
-    if [ $status -eq 0 ]
-        ssh-add ~/.ssh/amccullough-corp-20150911 ~/.ssh/amccullough-prod-20150910 ~/.ssh/github
-        if [ $status -eq 2 ]
-            start_agent
+        ssh-add -l | grep -e "The agent has no identities" > /dev/null
+        if [ $status -eq 0 ]
+                ssh-add ~/.ssh/amccullough-corp-20150911 ~/.ssh/amccullough-prod-20150910 ~/.ssh/github
+                if [ $status -eq 2 ]
+                        start_agent
+                end
         end
-    end
 end
 
 if [ -n "$SSH_AGENT_PID" ]
-    ps -ef | grep $SSH_AGENT_PID | grep ssh-agent > /dev/null
-    if [ $status -eq 0 ]
-        test_identities
-    end
+        ps -ef | grep $SSH_AGENT_PID | grep ssh-agent > /dev/null
+        if [ $status -eq 0 ]
+                test_identities
+        end
 else
-    if [ -f $SSH_ENV ]
-        . $SSH_ENV > /dev/null
-    end
-    ps -ef | grep $SSH_AGENT_PID |  grep [s]sh-agent > /dev/null
-    if [ $status -eq 0 ]
-        test_identities
-    else
-        start_agent
-    end
+        if [ -f $SSH_ENV ]
+                . $SSH_ENV > /dev/null
+        end
+        ps -ef | grep $SSH_AGENT_PID |  grep [s]sh-agent > /dev/null
+        if [ $status -eq 0 ]
+                test_identities
+        else
+                start_agent
+        end
 end
 
 set REPOS "svn+ssh://amccullough@svn.corp.imvu.com/home/svnrepos/trunk/operations/"
