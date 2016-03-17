81dc71591d3ae53cf5a3b9fea5a7821d2da63871
Adam McCullough <merlinfmct87@gmail.com>
2016-03-16 19:38:35 -0700

    Never enough helm!

--- emacs.d/site-start.d/40_helm.el
+++ emacs.d/site-start.d/40_helm.el
@@ -1,6 +1,7 @@
 ;;; Helm
 (require 'helm)
 (require 'helm-config)
+(require 'helm-grep)
 
 ;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
 ;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
@@ -11,8 +12,47 @@
 (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
 (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
 (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
+(define-key helm-grep-mode-map (kbd "<return>")  'helm-grep-mode-jump-other-window)
+(define-key helm-grep-mode-map (kbd "n")  'helm-grep-mode-jump-other-window-forward)
+(define-key helm-grep-mode-map (kbd "p")  'helm-grep-mode-jump-other-window-backward)
+
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-h SPC") 'helm-all-mark-rings)
+(global-set-key (kbd "C-c h o") 'helm-occur)
+
+(global-set-key (kbd "C-c h C-c w") 'helm-wikipedia-suggest)
+
+(global-set-key (kbd "C-c h x") 'helm-register)
+;; (global-set-key (kbd "C-x r j") 'jump-to-register)
+
+(define-key 'help-command (kbd "C-f") 'helm-apropos)
+(define-key 'help-command (kbd "r") 'helm-info-emacs)
+(define-key 'help-command (kbd "C-l") 'helm-locate-library)
+
+;; use helm to list eshell history
+(add-hook 'eshell-mode-hook
+          #'(lambda ()
+              (define-key eshell-mode-map (kbd "M-l")  'helm-eshell-history)))
+
+;;; Save current position to mark ring
+(add-hook 'helm-goto-line-before-hook 'helm-save-current-pos-to-mark-ring)
+
+;; show minibuffer history with Helm
+(define-key minibuffer-local-map (kbd "M-p") 'helm-minibuffer-history)
+(define-key minibuffer-local-map (kbd "M-n") 'helm-minibuffer-history)
+
+(define-key global-map [remap find-tag] 'helm-etags-select)
+
+(define-key global-map [remap list-buffers] 'helm-buffers-list)
+
+(require 'helm-swoop)
+
+;; Change the keybinds to whatever you like :)
+(global-set-key (kbd "C-c h o") 'helm-swoop)
+(global-set-key (kbd "C-c s") 'helm-multi-swoop-all)
+
 
 ;;; This seems to be the one place where Ido wins out over helm. Helm plus Tramp bring Emacs to a crawl.
 (require 'ido)
@@ -23,7 +63,6 @@
 (setq ido-vertical-show-count t)
 (global-set-key (kbd "C-c C-f") 'counsel-find-file)
 
-
 (global-unset-key (kbd "C-x b"))
 (global-set-key (kbd "C-x b") 'helm-mini)
 
@@ -31,14 +70,56 @@
   (setq helm-google-suggest-use-curl-p t))
 
 (setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      helm-candidate-number-limit 500 ; limit the number of displayed canidates
+      helm-buffers-fuzzy-matching t          ; fuzzy matching buffer names when non-nil
       helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
       helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
       helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
       helm-ff-file-name-history-use-recentf t)
 
+
+(add-to-list 'helm-sources-using-default-as-input 'helm-source-man-pages)
+
+(when (executable-find "curl")
+  (setq helm-google-suggest-use-curl-p t))
+
 (helm-mode 1)
 (eval-after-load 'flycheck
   '(define-key flycheck-mode-map (kbd "C-c ! h") 'helm-flycheck))
+
+
+(require 'helm-gtags)
+
+(setq
+ helm-gtags-ignore-case t
+ helm-gtags-auto-update t
+ helm-gtags-use-input-at-cursor t
+ helm-gtags-pulse-at-cursor t
+ helm-gtags-prefix-key "\C-cg"
+ helm-gtags-suggested-key-mapping t
+ )
+
+;; Enable helm-gtags-mode in Dired so you can jump to any tag
+;; when navigate project tree with Dired
+(add-hook 'dired-mode-hook 'helm-gtags-mode)
+
+;; Enable helm-gtags-mode in Eshell for the same reason as above
+(add-hook 'eshell-mode-hook 'helm-gtags-mode)
+
+;; Enable helm-gtags-mode in languages that GNU Global supports
+(add-hook 'c-mode-hook 'helm-gtags-mode)
+(add-hook 'c++-mode-hook 'helm-gtags-mode)
+(add-hook 'java-mode-hook 'helm-gtags-mode)
+(add-hook 'asm-mode-hook 'helm-gtags-mode)
+
+;; key bindings
+(define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
+(define-key helm-gtags-mode-map (kbd "C-j") 'helm-gtags-select)
+(define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
+(define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
+(define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
+(define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
+
 
 
 (require 'helm-anything)
@@ -47,8 +128,6 @@
 (require 'helm-descbinds)
 (helm-descbinds-mode)
 
-
-(define-key global-map (kbd "C-c l") 'helm-locate-library)
 
 (require 'helm-descbinds)
 (helm-descbinds-mode)
