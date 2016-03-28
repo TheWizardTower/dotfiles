aa2b7e855a3ae4a415e5d232bdcd4aba8bb9420c
Adam McCullough <merlinfmct87@gmail.com>
2016-03-28 02:24:35 -0700

    Seems like this set of configs *doesn't* crash. Need to poke at it further to make sure.

---
+++ emacs.d/jail-site-start.d/00_one_off.el
@@ -0,0 +1,62 @@
+(setq-default major-mode 'org-mode)
+
+(require 'dired+)
+
+;;; Stolen from http://efod.se/writings/linuxbook/html/date-and-time.html
+(setq display-time-24hr-format t)
+;; (setq display-time-day-and-date t)
+(display-time)
+
+(fset 'yes-or-no-p 'y-or-n-p)
+(load-theme 'evenhold t)
+
+(require 'browse-kill-ring+)
+
+(require 'undo-tree)
+(global-undo-tree-mode)
+
+;; Stolen from Sacha Chua.
+(setq undo-tree-visualizer-timestamps t)
+(setq undo-tree-visualizer-diff t)
+
+(eval-after-load "menu-bar" '(require 'menu-bar+))
+
+(setq-default indent-tabs-mode nil)
+
+;; Highlight matching parenthesis.
+(show-paren-mode 1)
+
+(require 'gnomenm)
+
+(require 'saveplace)
+(setq-default save-place t)
+
+(global-visual-line-mode)
+
+(require 'whitespace)
+(diminish 'whitespace-mode "ᗣ")
+(diminish 'global-whitespace-mode "ᗣ")
+(add-hook 'before-save-hook 'whitespace-cleanup)
+
+;; make whitespace-mode use just basic coloring
+;;(setq whitespace-style (quote (spaces tabs newline space-mark tab-mark newline-mark)))
+(setq whitespace-display-mappings
+      ;; all numbers are Unicode codepoint in decimal. ⁖ (insert-char 182 1)
+      '(
+        (space-mark 32 [183] [46]) ; 32 SPACE 「 」, 183 MIDDLE DOT 「·」, 46 FULL STOP 「.」
+        (newline-mark 10 [182 10]) ; 10 LINE FEED
+        (tab-mark 9 [9655 9] [92 9]) ; 9 TAB, 9655 WHITE RIGHT-POINTING TRIANGLE 「▷」
+        ))
+
+(require 'guru-mode)
+(guru-global-mode +1)
+
+
+(require 'ecb)
+
+(global-aggressive-indent-mode 1)
+
+(require 'insert-shebang)
+(add-hook 'find-file-hook 'insert-shebang)
+
+;; (require 'exercism)
--- emacs.d/site-start.d/00_one_off.el
+++ emacs.d/site-start.d/00_one_off.el
@@ -1,14 +1,12 @@
-(setq-default major-mode 'org-mode)
-
-(setq comment-auto-fill-only-comments t)
-
 (require 'dired+)
 
-(load-theme 'evenhold t)
+;;; Stolen from http://efod.se/writings/linuxbook/html/date-and-time.html
+(setq display-time-24hr-format t)
+;; (setq display-time-day-and-date t)
+(display-time)
 
-(require 'sane-term)
-(global-set-key (kbd "C-x t") 'sane-term)
-(global-set-key (kbd "C-x T") 'sane-term-create)
+(fset 'yes-or-no-p 'y-or-n-p)
+(load-theme 'evenhold t)
 
 (require 'browse-kill-ring+)
 
@@ -19,65 +17,9 @@
 (setq undo-tree-visualizer-timestamps t)
 (setq undo-tree-visualizer-diff t)
 
-(column-number-mode)
-
 (eval-after-load "menu-bar" '(require 'menu-bar+))
 
 (setq-default indent-tabs-mode nil)
 
 ;; Highlight matching parenthesis.
 (show-paren-mode 1)
-
-(require 'kv)
-(require 'gnomenm)
-
-(require 'saveplace)
-(setq-default save-place t)
-
-(global-visual-line-mode)
-
-(require 'whitespace)
-(diminish 'whitespace-mode "ᗣ")
-(diminish 'global-whitespace-mode "ᗣ")
-(add-hook 'before-save-hook 'whitespace-cleanup)
-
-;; make whitespace-mode use just basic coloring
-;;(setq whitespace-style (quote (spaces tabs newline space-mark tab-mark newline-mark)))
-(setq whitespace-display-mappings
-      ;; all numbers are Unicode codepoint in decimal. ⁖ (insert-char 182 1)
-      '(
-        (space-mark 32 [183] [46]) ; 32 SPACE 「 」, 183 MIDDLE DOT 「·」, 46 FULL STOP 「.」
-        (newline-mark 10 [182 10]) ; 10 LINE FEED
-        (tab-mark 9 [9655 9] [92 9]) ; 9 TAB, 9655 WHITE RIGHT-POINTING TRIANGLE 「▷」
-        ))
-
-(require 'guru-mode)
-(guru-global-mode +1)
-
-(require 'expand-region)
-(global-set-key (kbd "C-=") 'er/expand-region)
-
-
-;;; Stolen from http://efod.se/writings/linuxbook/html/date-and-time.html
-(setq display-time-24hr-format t)
-;; (setq display-time-day-and-date t)
-(display-time)
-
-(fset 'yes-or-no-p 'y-or-n-p)
-
-(require 'ecb)
-
-(global-aggressive-indent-mode 1)
-
-(require 'alert)
-
-(require 'blank-mode)
-
-(require 'indent-guide)
-
-(indent-guide-global-mode)
-
-(require 'insert-shebang)
-(add-hook 'find-file-hook 'insert-shebang)
-
-(require 'exercism)
