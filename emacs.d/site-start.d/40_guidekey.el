3a24a158017de8ef477824dbb812377eec9d5366
Adam McCullough <merlinfmct87@gmail.com>
2016-03-27 17:36:31 -0700

    Adding a guidemode prompt.

--- emacs.d/site-start.d/40_guidekey.el
+++ emacs.d/site-start.d/40_guidekey.el
@@ -1,3 +1,3 @@
 (require 'guide-key)
-(setq guide-key/guide-key-sequence '("C-x r" "C-x 5" "C-c" "C-c h"))
+(setq guide-key/guide-key-sequence '("C-x r" "C-x 5" "C-x 7" "C-c" "C-c h"))
 (guide-key-mode 1)
