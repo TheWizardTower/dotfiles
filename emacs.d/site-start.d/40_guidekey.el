3f68a003e3bcda4fef2059654022b8d8054e8488
Adam McCullough <merlinfmct87@gmail.com>
2016-03-28 00:05:19 -0700

    Guide-key mode for flycheck, please!

--- emacs.d/site-start.d/40_guidekey.el
+++ emacs.d/site-start.d/40_guidekey.el
@@ -1,3 +1,3 @@
 (require 'guide-key)
-(setq guide-key/guide-key-sequence '("C-x r" "C-x 5" "C-x 7" "C-c" "C-c h"))
+(setq guide-key/guide-key-sequence '("C-x r" "C-x 5" "C-x 7" "C-c" "C-c h") "C-c !")
 (guide-key-mode 1)
