8982c553c3417355c530433bd73469c444657df9
Adam McCullough <merlinfmct87@gmail.com>
2016-04-28 01:38:30 -0700

    re-ordered for alphabetical niceness.

--- installGoPackages.sh
+++ installGoPackages.sh
@@ -7,19 +7,20 @@ if [ "$USER" = "root" ]; then
    export HOMEDIR="/root/"
 fi
 
-
 export GOPATH="${HOMEDIR}/gocode"
 
-go get -u golang.org/x/tools/cmd/godoc
 go get -u golang.org/x/tools/cmd/cover
+sudo -E go get -u golang.org/x/tools/cmd/godoc
 go get -u golang.org/x/tools/cmd/gorename
+go get -u golang.org/x/tools/cmd/guru
 go get -u golang.org/x/tools/cmd/oracle
 go get -u golang.org/x/tools/cmd/vet
 
-go get -u github.com/motemen/gore
 go get -u github.com/alecthomas/gometalinter
+go get -u github.com/motemen/gore
 go get -u github.com/nsf/gocode
 go get -u github.com/peco/peco/cmd/peco
-go get -u golang.org/x/tools/cmd/guru
+go get -u github.com/rogpeppe/godef
+go get -u github.com/tokozedg/sman
 
 $HOMEDIR/gocode/bin/gometalinter --install --update
