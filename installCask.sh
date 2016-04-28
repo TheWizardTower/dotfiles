c49156aee3022be96b97291453ee0cd78f5edad1
Adam McCullough <merlinfmct87@gmail.com>
2016-04-28 02:08:00 -0700

    Obsolete now, but make it correct anyway.

--- installCask.sh
+++ installCask.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-if [ -d  ~/.cask/ ]; then
-    ~/.cask/bin/cask upgrade-cask
-else
-    curl -fsSL https://raw.githubusercontent.com/cask/cask/master/go | python
+if [ ! -d  ~/.cask/ ]; then
+    ln -s ~/.cask ~/dotfiles/cask
 fi
+    ~/.cask/bin/cask upgrade-cask
