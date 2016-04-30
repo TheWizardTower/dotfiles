a18a108fb50a38f2a6e32c4e8db288b19ab54da7
Adam McCullough <merlinfmct87@gmail.com>
2016-04-29 23:31:24 -0700

    Another sensible hotkey.

--- emacs.d/site-start.d/40_helm.el
+++ emacs.d/site-start.d/40_helm.el
@@ -21,6 +21,7 @@
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "C-h SPC") 'helm-all-mark-rings)
 (global-set-key (kbd "C-c h o") 'helm-occur)
+(global-set-key (kbd "C-c a") 'helm-ag)
 
 (global-set-key (kbd "C-c h C-c w") 'helm-wikipedia-suggest)
 
