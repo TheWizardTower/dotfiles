57f1b592f312e26800e8992983be661b6d5eb162
Adam McCullough <merlinfmct87@gmail.com>
2016-04-30 03:27:20 -0700

    Suuuuper useful things.

--- emacs.d/site-start.d/50_magit.el
+++ emacs.d/site-start.d/50_magit.el
@@ -1,9 +1,19 @@
-; (require 'magit)
+(require 'magit)
 (require 'magit-svn)
+(require 'browse-at-remote)
+(require 'git-messenger)
 
 (global-set-key (kbd "C-x G") 'magit-status)
+(global-set-key (kbd "C-c g") 'browse-at-remote/browse)
 
 (add-hook 'prog-mode-hook 'turn-on-diff-hl-mode)
 (add-hook 'magit-mode-hook 'magit-svn-mode)
 
 (global-magit-file-mode)
+
+(global-set-key (kbd "C-x v p") 'git-messenger:popup-message)
+
+(define-key git-messenger-map (kbd "m") 'git-messenger:copy-message)
+
+;; Enable magit-commit-mode after typing 's', 'S', 'd'
+(add-hook 'git-messenger:popup-buffer-hook 'magit-commit-mode)
