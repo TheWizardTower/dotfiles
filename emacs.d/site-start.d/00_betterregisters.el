4d8593a0e8a966f0a9b5d8441c8869289b91d646
Adam McCullough <merlinfmct87@gmail.com>
2016-06-08 03:11:09 -0700

    More register candy!

--- emacs.d/site-start.d/00_betterregisters.el
+++ emacs.d/site-start.d/00_betterregisters.el
@@ -4,3 +4,13 @@
 
 (require 'register-list)
 (define-key better-registers-r-map "l" 'list-registers)
+
+(require 'iregister)
+(global-set-key (kbd "M-n") 'iregister-jump-to-next-marker)
+(global-set-key (kbd "M-p") 'iregister-jump-to-previous-marker)
+(global-set-key (kbd "M-u") 'iregister-point-or-text-to-register)
+
+;; If region is active then `iregister-point-or-text-to-register' command stores a
+;; text to any empty register, otherwise it stores a point.
+
+(global-set-key (kbd "M-l") 'iregister-latest-text)
