a93b6663fe0b27e4abf8c8b43407be66d89c8a09
Adam McCullough <merlinfmct87@gmail.com>
2016-07-29 15:20:20 -0700

    Updated for my new 4k screen.

--- screenlayout/work.sh
+++ screenlayout/work.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VGA-0 --mode 1920x1200 --pos 544x0 --rotate left --output LVDS-0 --mode 1920x1080 --pos 4144x474 --rotate normal --output DP-5 --mode 1920x1200 --pos 2944x14 --rotate left --output DP-4 --off --output DP-3 --mode 1920x1200 --pos 1744x14 --rotate left --output DP-2 --off --output DP-1 --off --output DP-0 --off
+xrandr --output VGA-0 --primary --mode 1920x1200 --pos 0x240 --rotate left --output DP-6 --off --output DP-5 --mode 1920x1200 --pos 2400x240 --rotate left --output DP-4 --mode 3840x2160 --pos 3600x0 --rotate normal --output DP-3 --mode 1920x1200 --pos 1200x240 --rotate left --output DP-2 --off --output DP-1 --off --output DP-0 --off
