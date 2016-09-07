1bff87186ff06ae3d54345891af8ce7e40776836
Adam McCullough <merlinfmct87@gmail.com>
2016-09-07 01:56:10 -0700

    Default font for 4k monitors.

--- emacs.d/site-start.d/99_custom_set_variables.el
+++ emacs.d/site-start.d/99_custom_set_variables.el
@@ -23,7 +23,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 83 :width normal :foundry "unknown" :family "Inconsolata"))))
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 151 :width normal :foundry "PfEd" :family "Inconsolata"))))
  '(col-highlight ((t (:background "gray15"))))
  '(company-scrollbar-bg ((t (:background "#191919"))))
  '(company-scrollbar-fg ((t (:background "#0c0c0c"))))
