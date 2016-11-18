bf35ec7cf68e9ba74bf03c9963dfa808d6c3102a
Adam McCullough <merlinfmct87@gmail.com>
2016-11-18 13:46:08 -0800

    Library no longer in marmalade/melpa?

--- emacs.d/site-start.d/00_mark.el
+++
@@ -1,7 +0,0 @@
-(require 'mark)
-
-;; This would probably be more appropriate or useful as a Hydra.
-(global-set-key (kbd "<C-s-right>") 'forward-mark)
-(global-set-key (kbd "<C-s-left>") 'backward-mark)
-(global-set-key (kbd "<C-s-down>") 'show-marks)
-
