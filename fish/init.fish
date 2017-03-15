e0b0b4e732dcec6286310cb1a8dceb323911ecc6
Adam McCullough <merlinfmct87@gmail.com>
2017-03-14 23:33:09 -0700

    Overhauled my fish setup. Need to setup the install for this, too.

--- fish/init.fish
+++ fish/init.fish
@@ -1,65 +1,12 @@
 set -gx EDITOR "emacsclient -c"
 set -gx TERM "screen-256color"
 set -gx PERL5LIB "$HOME/imvu/operations/software/ImvuPerlModules"
-set -gx PATH $HOME/bin $HOME/.local/bin $HOME/.cask/bin $HOME/gocode/bin $PATH
-set -gx GOPATH $HOME/gocode/:$HOME/imvu/operations/golang/imvu:$HOME/imvu/operations/golang/external:/usr/lib/go
 set -gx cabal_helper_libexecdir $HOME/.local/bin
 
 function fish_title
         true
 end
 
-# All built-in plugins can be found at ~/.oh-my-fish/plugins/
-# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
-# Enable plugins by adding their name separated by a space to the line below.
-
-
-setenv SSH_ENV $HOME/.ssh/environment
-setenv SSH_SOCK_LINK $HOME/.ssh/auth_sock_symlink
-
-function start_agent
-        echo "Initializing new SSH agent ..."
-        set ssh_eval (/usr/bin/ssh-agent -c)
-        echo $ssh_eval > $SSH_ENV
-        echo "succeeded"
-        chmod 600 $SSH_ENV
-        . $SSH_ENV
-        ssh-add ~/.ssh/amccullough-corp-20150911 ~/.ssh/amccullough-prod-20150910 ~/.ssh/github
-        ln -sf $SSH_AUTH_SOCK $SSH_SOCK_LINK
-end
-
-function test_identities
-        ssh-add -l | grep -e "The agent has no identities" > /dev/null
-        if [ $status -eq 0 ]
-                ssh-add ~/.ssh/amccullough-corp-20150911 ~/.ssh/amccullough-prod-20150910 ~/.ssh/github
-                if [ $status -eq 2 ]
-                        start_agent
-                end
-        end
-end
-
-if [ -n "$SSH_AGENT_PID" ]
-        ps -ef | grep $SSH_AGENT_PID | grep ssh-agent > /dev/null
-        if [ $status -eq 0 ]
-                test_identities
-        end
-else
-        if [ -f $SSH_ENV ]
-                . $SSH_ENV > /dev/null
-        end
-        ps -ef | grep $SSH_AGENT_PID |  grep [s]sh-agent > /dev/null
-        if [ $status -eq 0 ]
-                test_identities
-        else
-                start_agent
-        end
-end
-
-set REPOS "svn+ssh://amccullough@svn.corp.imvu.com/home/svnrepos/trunk/operations/"
 
 alias sandbox="su -c  'ssh -A -L 80:localhost:80 -L 443:localhost:443 -L 11211:localhost:11211 -L 3306:localhost:3306 cit@192.168.112.13'"
 
-set -gx ENHANCD_FILTER fzf
-
-source ~/.enhancd/fish/enhancd.fish
-alias cd cd::cd
