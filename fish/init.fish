7b52ff8efb0b6f00d0e52b94b9ab8fe4f9597b1f
Adam McCullough <merlinfmct87@gmail.com>
2017-03-15 01:10:25 -0700

    sandbox is obsolete, and the vars are in paths.d

--- fish/init.fish
+++ fish/init.fish
@@ -1,12 +1,3 @@
-set -gx EDITOR "emacsclient -c"
-set -gx TERM "screen-256color"
-set -gx PERL5LIB "$HOME/imvu/operations/software/ImvuPerlModules"
-set -gx cabal_helper_libexecdir $HOME/.local/bin
-
 function fish_title
         true
 end
-
-
-alias sandbox="su -c  'ssh -A -L 80:localhost:80 -L 443:localhost:443 -L 11211:localhost:11211 -L 3306:localhost:3306 cit@192.168.112.13'"
-
